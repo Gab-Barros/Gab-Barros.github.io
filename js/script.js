e95dec29e4a6f6c89df1b5ab03496701d405ecc8
Gabriel Barros <gabrielalcantarabarros524@gmail.com>
2021-04-10 06:11:10 -0300

    efeito fade adicionado a section skills. Cor secundaria alterada no CSS.

--- js/script.js
+++ js/script.js
@@ -23,3 +23,45 @@ function fadeInDown() {
   }
 }
 fadeInDown();
+
+// Fade Left Skills
+function fadeLeftSkills() {
+  const elements = document.querySelectorAll("[data-fadeInLeft]");
+  console.log(elements);
+  const windowHeight = Math.floor(window.innerHeight * 0.8);
+  if (elements.length) {
+    window.addEventListener("load", checkDistance);
+
+    // Pega a distância do elemento até o topo da página
+    function checkDistance() {
+      elements.forEach((element) => {
+        const distElementTop = element.offsetTop;
+        //ativa a função passando a distancia do elemento ao topo da página como argumento
+        activeClass(distElementTop, element);
+      });
+    }
+
+    function activeClass(distElementTop, element) {
+      function debounce(func, time) {
+        let timer = null;
+        return function () {
+          clearTimeout(timer);
+          timer = setTimeout(func, time);
+        };
+      }
+
+      window.addEventListener(
+        "scroll",
+        debounce(function () {
+          if (
+            window.pageYOffset + windowHeight > distElementTop &&
+            !(window.pageYOffset + windowHeight > distElementTop * 1.3)
+          ) {
+            element.classList.add("active");
+          }
+        }, 100)
+      );
+    }
+  }
+}
+fadeLeftSkills();
